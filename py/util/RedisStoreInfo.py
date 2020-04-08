3b7352828f71e7e9c1df75fc2675bfc86b62b96f
jun0315 <1072505283@qq.com>
2020-04-08 16:15:37 +0800

    add redis process class

---
+++ py/util/RedisStoreInfo.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup
+import redis
+
+
+class RedisStoreInfo(object):
+    def __init__(self):
+        self.redis_pool = redis.ConnectionPool(host='localhost', port=6379, password='root', db=0)
+        self.redis_conn = redis.Redis(connection_pool=self.redis_pool)
+
+        soup = BeautifulSoup(open('202002281436.html', encoding='utf-8'), features='html.parser')
+        city_infos = soup.find('tbody').findAll('tr')
+        for index in range(len(city_infos)):
+            if index < 3:
+                continue
+            city_detail = city_infos[index].findAll('td')
+            # 去除首位空格
+            self.redis_conn.set(city_detail[2].text.strip(), city_detail[1].text)
+
+    def get_xzqh(self, city_name):
+        return self.redis_conn.get(city_name).decode()
+
+
+# demo
+
+r = RedisStoreInfo()
+print(r.get_xzqh("北京市"))
+print(r.get_xzqh("淮南市"))
